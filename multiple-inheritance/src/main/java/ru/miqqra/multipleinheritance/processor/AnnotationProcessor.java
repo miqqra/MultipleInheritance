bdb1d9b5f208f52b33ae868f596a02729434bf06
miqqra <mikhail.krasnikov.04@gmail.com>
2024-03-13 00:25:00 +0700

    Some fixes

--- multiple-inheritance/src/main/java/ru/miqqra/multipleinheritance/processor/AnnotationProcessor.java
+++ multiple-inheritance/src/main/java/ru/miqqra/multipleinheritance/processor/AnnotationProcessor.java
@@ -1,12 +1,12 @@
 package ru.miqqra.multipleinheritance.processor;
 
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.CodeBlock;
 import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
-
-import javax.lang.model.element.Modifier;
-import javax.lang.model.type.MirroredTypesException;
+import static javax.lang.model.element.Modifier.PUBLIC;
 import ru.miqqra.multipleinheritance.annotations.Inheritance;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -15,10 +15,17 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.util.Elements;
+import javax.tools.Diagnostic;
 import java.io.IOException;
-import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 @SupportedAnnotationTypes("ru.miqqra.multipleinheritance.annotations.Inheritance")
@@ -31,66 +38,169 @@ public class AnnotationProcessor extends AbstractProcessor {
         if (annotations.isEmpty()) {
             return false;
         }
+        Elements elements = processingEnv.getElementUtils();
         Set<? extends Element> classes = roundEnv.getElementsAnnotatedWith(Inheritance.class);
         for (var element : classes) {
-            createImplementationFile((TypeElement) element);
+            String packageName = "org.example";
+            String className = element.getSimpleName().toString();
+
+            TypeSpec.Builder implementationClass = TypeSpec.classBuilder(className + "Impl")
+                    .addModifiers(PUBLIC)
+                    .superclass(TypeName.get(element.asType()));
+
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "AAAAAAAAAAA");
+            //processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, element.asType().toString());
+            //processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, element.getAnnotationMirrors().stream().map(Objects::toString).reduce((a, b) -> a + " " + b).orElse(null));
+
+            for (var mirror : element.getAnnotationMirrors()) {
+                DeclaredType annotationType = mirror.getAnnotationType();
+                Element annotationDecl = annotationType.asElement();
+
+//                try {
+//                    processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, element.asType().toString());
+//                    Class<?> a = Class.forName(element.asType().toString());
+//                    processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, a.getSimpleName());
+////                    var ann = a.getAnnotation(Inheritance.class);
+////                    ann.classes();
+////                    processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, ann.classes().toString());
+//                } catch (ClassNotFoundException e) {
+//                    throw new RuntimeException(e);
+//                }
+
+//                Map<? extends ExecutableElement, ? extends AnnotationValue> values =
+//                        elements.getElementValuesWithDefaults(mirror);
+//
+//                var classes1 = (Class<?>[]) getValue(values, "classes");
+//
+//                processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, classes1.toString());
+//
+//                for (var clazz : classes1) {
+//                    var field = FieldSpec.builder(clazz.getClass(), " ").addModifiers(PRIVATE).build();
+//                }
+
+//                List<FieldSpec> fields = classes1.stream()
+//                        .map(v ->
+//                                //FieldSpec.builder(String.class, "aa")
+//                                //.addModifiers(PRIVATE)
+//                                //        .build()
+//                        )
+//                        .toList();
+
+                //fields.forEach(implementationClass::addField);
+            }
+
+
+            for (Element methodElement : element.getEnclosedElements()) {
+                if (methodElement.getKind() == ElementKind.METHOD) {
+                    MethodSpec.Builder methodBuilder = MethodSpec.overriding((ExecutableElement) methodElement)
+                            .addModifiers(PUBLIC)
+                            .addCode(CodeBlock.of("System.out.println(\"hello\");"));
+                    // TODO: Add method body according to your requirements
+                    // You can use methodBuilder.addStatement("your code here");
+
+                    implementationClass.addMethod(methodBuilder.build());
+                }
+            }
+
+            TypeSpec implementation = implementationClass.build();
+            JavaFile javaFile = JavaFile.builder(packageName, implementation).build();
+
+            try {
+                javaFile.writeTo(processingEnv.getFiler());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return true;
     }
 
-    private void createImplementationFile(TypeElement inheritedClass) {
-        List<TypeElement> parents;
-        try {
-            //noinspection ResultOfMethodCallIgnored
-            inheritedClass.getAnnotation(Inheritance.class).classes();
-            return;
-        } catch (MirroredTypesException mte) {
-            parents = mte.getTypeMirrors().stream()
-                .map(x -> (TypeElement) processingEnv.getTypeUtils().asElement(x)).toList();
+    private Class<?>[] getValue(Map<? extends ExecutableElement,
+            ? extends AnnotationValue> values,
+                                String name) {
+        for (Map.Entry<? extends ExecutableElement,
+                ? extends AnnotationValue> e : values.entrySet()) {
+            if (name.contentEquals(e.getKey().getSimpleName()))
+                return (Class<?>[]) e.getValue().getValue();
         }
+        return null;
+    }
 
-//        ElementFilter.fieldsIn(List.of(parents.get(0)));
+    private void createImplementationFile(TypeElement typeElement, Element element) {
+        String packageName = "org.example";
+        String className = typeElement.getSimpleName().toString();
 
-
-        TypeSpec.Builder implementationClass = TypeSpec.classBuilder(
-                inheritedClass.getSimpleName().toString() + "Impl")
-            .addModifiers(inheritedClass.getModifiers().toArray(new Modifier[0]))
-//            .superclass((TypeName) inheritedClass)
-            .superclass(TypeName.get(inheritedClass.asType()));
-        implementationClass.addJavadoc("Parent classes: " + String.join(", ",
-            parents.stream().map(TypeElement::toString).toList()));
-
+        TypeSpec.Builder implementationClass = TypeSpec.classBuilder(className + "Impl")
+                .addModifiers(PUBLIC)
+                .superclass(TypeName.get(typeElement.asType()));
 
 //        //todo
-//        Arrays.stream(inheritedClass.getClass().getMethods()).filter(v -> v.isAnnotationPresent(Inherit.class));
+//        Arrays.stream(typeElement.getClass().getMethods()).filter(v -> v.isAnnotationPresent(Inherit.class));
 //
-//        List<FieldSpec> fields1 = Arrays.stream(inheritedClass
-//                .getAnnotation(Inheritance.class)
-//                .classes())
-//            .map(v -> FieldSpec.builder(v, v.getSimpleName())
-//                .addModifiers(PRIVATE)
-//                .build())
-//            .toList();
+
+        //String a = String.valueOf(Arrays.stream(typeElement.getClass().getFields()).map(f -> f.getAnnotation(Inheritance.class)).filter(Objects::nonNull).map(v -> v.classes()).findFirst());
+
+        processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "AAAAAAAAAAA");
+        processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, typeElement.asType().toString());
+        processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, typeElement.getAnnotationMirrors().stream().map(Objects::toString).reduce((a, b) -> a + " " + b).orElse(null));
+
+        //processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, classes.toString());
+
+//
+//        Arrays.stream(element.getClass().getAnnotation(Inheritance.class).classes())
+//                .peek(v -> processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, v.toString()))
+//                .toList();
+
+//                .get(0)
+//                .getElementValues()
+//                .values()
+//                .stream()
+//                .findFirst()
+//                .stream()
+//                .map(v -> {
+//                    try {
+//                        return Class.forName(v.toString());
+//                    } catch (ClassNotFoundException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                })
+//                .map(Class::getSimpleName)
+//                .toString());
+
+
+//        List<FieldSpec> fields = typeElement.getAnnotationMirrors().get(0)
+//                .getElementValues()
+//                .values()
+//                .stream()
+//                .findFirst()
+//                .map(v -> {
+//                    try {
+//                        return Class.forName(String.valueOf(v));
+//                    } catch (ClassNotFoundException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                })
+//                .map(v -> FieldSpec.builder(v, v.toString())
+//                        .addModifiers(PRIVATE)
+//                        .build())
+//                .stream().toList();
 //
 //        fields.forEach(implementationClass::addField);
 
-//        for (Element methodElement : inheritedClass.getEnclosedElements()) {
-//            if (methodElement.getKind() == ElementKind.METHOD) {
-//                MethodSpec.Builder methodBuilder =
-//                    MethodSpec.overriding((ExecutableElement) methodElement)
-//                        .addModifiers(PUBLIC)
-//                        .addCode(CodeBlock.of("\n" +
-//                            "                System.out.println(\"hello\");"));
-//                // TODO: Add method body according to your requirements
-//                // You can use methodBuilder.addStatement("your code here");
-//
-//                implementationClass.addMethod(methodBuilder.build());
-//            }
-//        }
+        for (Element methodElement : typeElement.getEnclosedElements()) {
+            if (methodElement.getKind() == ElementKind.METHOD) {
+                MethodSpec.Builder methodBuilder = MethodSpec.overriding((ExecutableElement) methodElement)
+                        .addModifiers(PUBLIC)
+                        .addCode(CodeBlock.of("\n" +
+                                "                System.out.println(\"hello\");"));
+                // TODO: Add method body according to your requirements
+                // You can use methodBuilder.addStatement("your code here");
+
+                implementationClass.addMethod(methodBuilder.build());
+            }
+        }
 
         TypeSpec implementation = implementationClass.build();
-        JavaFile javaFile =
-            JavaFile.builder(inheritedClass.getQualifiedName() + "Impl", implementation).build();
+        JavaFile javaFile = JavaFile.builder(packageName, implementation).build();
 
         try {
             javaFile.writeTo(processingEnv.getFiler());
